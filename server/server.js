aa9ac1051054418319875135c440e60d69cbadcf
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-05 11:58:47 -0500

    add dotenv

--- server/server.js
+++ server/server.js
@@ -2,16 +2,31 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const axios = require('axios');
+//dotenv to inject environment variable
+require('dotenv').config();
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
+app.get('/trending', (req, res) => {
+
+        axios
+        .get(`http://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}`)
+        .then((response) => { //GET arrives in the server
+            console.log(response.data);
+            res.send(response.data);//to send to client side
+        }).catch((err) => {
+            console.log(err);
+        })
+    })
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
     console.log('Listening on port: ', PORT);
-});
+})
+
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,33 @@
 
 import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
-function App () {
+function App() {
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">APIS</h1>
-          <h4><i>APIS</i></h4>
-        </header>
-        <br/>
-      </div>
-    );
-  
+  useEffect(() => {
+    getTrending();
+  }, [])
+
+  const getTrending = () => {
+    axios.get('/trending')
+      .then(response => {
+        console.log(response.data);
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">APIS</h1>
+        <h4><i>APIS</i></h4>
+      </header>
+      <br />
+    </div>
+  );
+
 }
 
 export default App;
